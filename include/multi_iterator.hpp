6bb59f1543e700efb88d15e3a500dbda45a1cc7b
Matthew Okenyi <matthew.okenyi@gmail.com>
2023-11-18 15:46:53 +0000

    Initial commit

---
+++ include/multi_iterator.hpp
@@ -0,0 +1,13 @@
+#ifndef MULTI_ITERATOR_HPP
+#define MULTI_ITERATOR_HPP
+
+// A version numpy's Array Iterator which simultaneously iterates over a
+// set of xtensors.
+// 
+// T: type of data held by the arrays being iterated.
+template<typename T>
+class multi_iterator {
+  
+}
+
+#endif // MULTI_ITERATOR_HPP
