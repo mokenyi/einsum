a143f4b04807cd51a81ec0648c1c70baed8d721c
Matthew Okenyi <matthew.okenyi@gmail.com>
2025-01-04 21:12:32 +0000

    Develop get_combined_dims_view overload for subscripts with repeated labels

--- include/get_combined_dims_view.hpp
+++ include/get_combined_dims_view.hpp
@@ -13,11 +13,44 @@ typename std::enable_if<
     typename I::tuple_type
   >::value,
   xt::xstrided_view<xt::xclosure_t<E const&>,typename E::shape_type,xt::layout_type::dynamic>
->::type get_combined_dims_view(xt::xexpression<E> const& x) {
+>::type get_combined_dims_view(xt::xexpression<E> const& x, int ellipsis) {
   std::cout << "has repeated label" << std::endl;
-  auto sh = x.derived_cast().shape();
-  auto st = x.derived_cast().strides();
-  return xt::strided_view(x.derived_cast(), std::move(sh), std::move(st), 0LU, xt::layout_type::dynamic);
+  auto y = x.derived_cast();
+  int const ndim = x.dimension();
+  std::vector<int> const labels = I::parse_operand_subscripts(x, ellipsis);
+  std::vector<int> icombinemap(ndim, -1);
+
+  int icombine(0);
+  std::vector<size_t> new_dims;
+  std::vector<int> new_strides;
+
+  int const ndim = labels.size();
+  for (int idim = 0; idim < ndim; ++idim) {
+    int const label = labels.at(idim);
+    int const dim = y.shape(idim);
+    int const stride = y.strides()[idim];
+
+    if (label >= 0) {
+      icombinemap.at(idim) = icombine;
+      new_dims.push_back(dim);
+      new_strides.push_back(stride);
+      ++icombine;
+    }
+    else {
+      int const i = icombinemap.at(idim + label);
+      icombinemap.at(idim) = -1;
+      if (new_dims.at(i) != dim) {
+        std::stringstream ss;
+        ss << "Cannot collapse dimension " << idim << " (of size ";
+        ss << dim << ") into dimension " << idim + label << " (of size ";
+        ss << new_dims.at(i) << ") for subscripts " << I::to_vector();
+        throw std::invaid_argument(ss.str());
+      }
+      new_strides.at(i) += stride;
+    }
+  }
+
+  return xt::strided_view(y, std::move(new_dims), std::move(new_strides), 0LU, xt::layout_type::dynamic);
 }
 
 template<typename I, typename E>
@@ -28,7 +61,7 @@ typename std::enable_if<
     typename I::tuple_type
   >::value,
   xt::xexpression<E> const&
->::type get_combined_dims_view(xt::xexpression<E> const& x) {
+>::type get_combined_dims_view(xt::xexpression<E> const& x, int ellipsis) {
   std::cout << "no repeated label" << std::endl;
   return x;
 }
