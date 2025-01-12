55cf65b2b60b64accf7321f9783ecc0e9dabee12
Matthew Okenyi <matthew.okenyi@gmail.com>
2025-01-12 12:51:07 +0000

    Add test_get_output_labels test case

--- test/src/test_get_output_labels.cpp
+++ test/src/test_get_output_labels.cpp
@@ -6,6 +6,23 @@
 #include "subscripts.hpp"
 #include "labels.hpp"
 
+TEST_CASE("Throw an exception if an output label does not appear in the input "
+  "labels") {
+  using out_labels = subscripts<A,B,C>;
+  std::vector<std::array<int,2>> label_counts;
+  label_counts.push_back(std::array<int,2>({A,1}));
+  label_counts.push_back(std::array<int,2>({B,1}));
+
+  CHECK_THROWS_WITH_AS(
+    get_output_labels<out_labels>(label_counts,0,_),
+    "Output subscripts contain label 3 which does not appear in input subscripts",
+    std::invalid_argument
+  );
+}
+
+
+
+
 // Uncommenting this case causes compilation to fail.
 //TEST_CASE("get_output_labels should violate an assertion if the labels have "
 //  "repeated values") {
