8809297cb25adb38af091b8b69d12e309a65d511
Matthew Okenyi <matthew.okenyi@gmail.com>
2025-01-12 13:45:28 +0000

    Add happy path test case to test_get_output_labels

--- test/src/test_get_output_labels.cpp
+++ test/src/test_get_output_labels.cpp
@@ -6,6 +6,33 @@
 #include "subscripts.hpp"
 #include "labels.hpp"
 
+TEST_CASE("Returns output labels when broadcast dims are places in the middle "
+  "of the output array's shape") {
+  using out_labels = subscripts<A,B,_,C>;
+  std::vector<std::array<int,2>> label_counts;
+  label_counts.push_back(std::array<int,2>({A,1}));
+  label_counts.push_back(std::array<int,2>({B,1}));
+  label_counts.push_back(std::array<int,2>({C,1}));
+  label_counts.push_back(std::array<int,2>({D,2}));
+
+  int const ndim_broadcast(4);
+  int const ellipsis(_);
+
+  std::vector<int> const actual = get_output_labels<out_labels>(
+    label_counts,
+    ndim_broadcast,
+    ellipsis
+  );
+
+  std::vector<int> expected;
+  expected.push_back(A);
+  expected.push_back(B);
+  for (int i=0; i<ndim_broadcast; ++i) { expected.push_back(_); }
+  expected.push_back(C);
+
+  CHECK(actual == expected);
+}
+
 TEST_CASE("Throw an exception if an output label does not appear in the input "
   "labels") {
   using out_labels = subscripts<A,B,C>;
@@ -19,9 +46,6 @@ TEST_CASE("Throw an exception if an output label does not appear in the input "
     std::invalid_argument
   );
 }
-
-
-
 
 // Uncommenting this case causes compilation to fail.
 //TEST_CASE("get_output_labels should violate an assertion if the labels have "
