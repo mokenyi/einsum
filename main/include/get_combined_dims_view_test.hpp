42e5206aebc4061ae0e875ac4f72948792ccfe30
Matthew Okenyi <matthew.okenyi@gmail.com>
2025-01-18 21:22:22 +0000

    Remove obsolete headers

--- main/include/get_combined_dims_view_test.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef GET_COMBINED_DIMS_VIEW_TEST_HPP
-#define GET_COMBINED_DIMS_VIEW_TEST_HPP
-
-void get_combined_dims_view_test();
-
-#endif // GET_COMBINED_DIMS_VIEW_TEST_HPP
--- main/include/multi_iterator_test.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef MULTI_ITERATOR_TEST_HPP
-#define MULTI_ITERATOR_TEST_HPP
-
-void multi_iterator_test();
-
-#endif // MULTI_ITERATOR_TEST_HPP
-
